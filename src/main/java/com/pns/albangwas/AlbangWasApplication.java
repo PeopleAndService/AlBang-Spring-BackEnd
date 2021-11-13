76518eca613c409c2b5f52266f8ad94609cf80a8
leesh96 <suho2718@gmail.com>
2021-11-13 18:37:09 +0900

    initial spring project

---
+++ src/main/java/com/pns/albangwas/AlbangWasApplication.java
@@ -0,0 +1,13 @@
+package com.pns.albangwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbangWasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbangWasApplication.class, args);
+	}
+
+}
